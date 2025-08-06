131644d252780a7ddbfd8ed8715131cd3df2cd64
Nair-Abhinav <abhinavnair7404@gmail.com>
2025-08-06 10:05:33 +0530

    added backend url

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendUrl: "http://localhost:8080", // Update with your actual backend URL
+    backendUrl: "https://ai-fitness-trainer-e834.onrender.com", // Update with your actual backend URL
   };
   
 export default config;
